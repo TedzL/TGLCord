4bb69050d06ae73767980f49f596eee02ffba890
TedzL <ted@greenfish.net>
2022-07-30 15:16:14 +1000

    yay

--- dist/typings/Events.d.ts
+++ dist/typings/Events.d.ts
@@ -1,6 +1,7 @@
 import { ClientEvents } from "discord.js";
+import { Client } from "../client/Client";
 declare type ValueOf<T> = T[keyof T];
 export declare type EventID = keyof ClientEvents;
 export declare type EventArgs = ValueOf<ClientEvents>;
-export declare type EventHandle = (...args: any) => void;
+export declare type EventHandle = (client: Client, ...args: any) => void;
 export {};
--- src/structures/Event.ts
+++ src/structures/Event.ts
@@ -1,5 +1,4 @@
 import { ClientEvents } from "discord.js";
-import { Client } from "../client/Client";
 import { EventHandle } from "../typings/Events";
 
 export class Event {
